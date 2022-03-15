466451f37b07141adb080d8cfb798506696bd41f
Nils Backe <nilsbacke@Nilss-MacBook-Pro.local>
2022-03-15 13:57:57 -0400

    a6 React.js sp22

--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import NavigationSidebar from "./NavigationSidebar";
+import WhoToFollowListItem from "./WhoToFollowList/who-to-follow-list-item";
+import PostSummaryList from "./PostSummaryList";
 import ExploreScreen from "./ExploreScreen/explore-screen";
 
 const Tuiter = () => {
